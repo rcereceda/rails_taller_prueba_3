8362d91ce32491a4a5fdd656779963099ac7e3c0
Rodrigo Cereceda <rcereced@gmail.com>
2016-07-28 20:33:22 -0300

    model testing & seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Catalog.destroy_all
+User.destroy_all
+Pokemon.destroy_all
 
-10.times do
-	Catalog.create(name: Faker::Pokemon.name, description: Faker::Lorem.sentence)
-end
+# 10.times do
+# 	Catalog.create(name: Faker::Pokemon.name, description: Faker::Lorem.sentence)
+# end
+
+users = User.create([{ email: 'rcereced@gmail.com', name: 'trainer1', password: '123456' }, { email: 'rodrigo@desafiolatam.com', name: 'admin', password: '123456' }])
+
+catalogs = Catalog.create([{ name: 'Ratata', description: 'Mouse Pokemon' }, { name: 'Caterpie', description: 'Worm Pokemon'}])
+
+Pokemon.create([{ user: users.first, catalog: catalogs.first }, { user: users.first, catalog: catalogs.first }, { user: users.first, catalog: catalogs.second }])
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@user = users(:one)
+  end
+
+  test "user one must have 3 pokemons" do
+  	assert_equal @user.pokemons.count, 3
+  end
+
+  test "user trainer1 must have a caterpie" do
+  	assert_includes @user.catalogs, catalogs(:two)
+  end
 end
