3182d77945510d3965313870fc4850e385844aad
Rodrigo Cereceda <rcereced@gmail.com>
2016-07-28 21:32:02 -0300

    devise sign up, log in, log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  get 'pokemons/index'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   resources :catalogs
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'catalogs#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
